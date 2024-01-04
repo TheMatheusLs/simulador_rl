3e110d7193ebd3fd6bffcb428edb8869442322df
Hermano Cabral <hcabral00@gmail.com>
2024-01-04 11:47:01 -0300

    Added periodic callback to the simulator

--- RL/simulator.py
+++ RL/simulator.py
@@ -2,7 +2,9 @@ class EpisodicSimulator:
     def __init__(self, env, policy, episode_size,
                  report_active=False, report_every=100000,
                  terminate_on_eoe=False,
-                 process_done_cb=lambda x, y: None):
+                 process_done_cb=None,
+                 callback_every=100000,
+                 callback_fun=None):
         self.env, self.policy = env, policy
         self.episode_size = episode_size
         self.report_active, self.report_every = report_active, report_every
@@ -11,12 +13,18 @@ class EpisodicSimulator:
         # episode_size steps
         self.terminate_on_eoe = terminate_on_eoe
         self.process_done = process_done_cb
+        # Function callback to be called every so often between runs
+        # of the simulation
+        self.cb_every, self.cb_fun = callback_every, callback_fun
 
     def run(self, nbr_runs):
         for run in range(nbr_runs):
             if self.report_active and \
                run % self.report_every == 0:
                 print(f"Round {run:4d}")
+
+            if self.cb_fun and (run + 1) % self.cb_every == 0:
+                self.cb_fun(run)
 
             st, _ = self.env.reset()
             action = self.policy.action(st)
@@ -27,7 +35,8 @@ class EpisodicSimulator:
                 self.policy.update(old_st, action, reward)
                 action = self.policy.action(st)
                 if done:
-                    self.process_done(run, step)
+                    if self.process_done:
+                        self.process_done(run, step)
                     if self.terminate_on_eoe:
                         break
 
@@ -56,5 +65,7 @@ if __name__ == "__main__":
 
     sim = EpisodicSimulator(MyEnvironment(), MyPolicy(),
                             episode_size=250,
-                            report_active=True, report_every=10000)
-    sim.run(100000)
+                            report_active=True, report_every=50,
+                            callback_every=1,
+                            callback_fun=lambda x: print(f"Run: {x}"))
+    sim.run(100)
